41282b88a53440d049e472097c3394c04594b7a3
Amon Ochuka <51824139+Amonoff@users.noreply.github.com>
2019-09-09 17:06:38 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,4 +4,13 @@ df = pd.read_csv('airbnb.csv')
 #we can first load and view the data
 df.head()
 
+#check for missing data
+missing_data = df.isnull()
+missing_data.head()
 
+#check per column for the whole data set
+for column in missing_data.columns.values.tolist():
+    print(column)
+    print(missing_data[column].value_counts())
+    print('')
+    
