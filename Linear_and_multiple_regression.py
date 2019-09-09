5fa6630554f194bd0f4d914196669f7fe6479587
Amon Ochuka <51824139+Amonoff@users.noreply.github.com>
2019-09-09 17:18:34 +0300

    Update Linear_and_multiple_regression.py

--- Linear_and_multiple_regression.py
+++ Linear_and_multiple_regression.py
@@ -1,3 +1,5 @@
+#First, without training and testing data
+#simple linear regression
 from sklearn.linear_model import LinearRegression
 #simple linear regression
 lr = LinearRegression()
@@ -8,3 +10,12 @@ Yhat = lr.predict(X)
 Yhat[0:5]
 #in the form of y = a + bx
 lr.intercept_
+lr.coef_
+
+#multiple regression
+Z = df[['number_of_reviews', 'minimum_nights', 'availability_365']]
+lr.fit(Z, df['price'])
+Yhat = lr.predict(Z)
+Yhat[0:5]
+lr.intercept_
+lr.coef_
