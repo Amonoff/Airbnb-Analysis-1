44d23c2bc383374215ffd04a98ea8c8bf311b6c1
Amon Ochuka <51824139+Amonoff@users.noreply.github.com>
2019-09-09 17:39:59 +0300

    Update Linear_and_multiple_regression.py

--- Linear_and_multiple_regression.py
+++ Linear_and_multiple_regression.py
@@ -23,3 +23,12 @@ lr.coef_
 #Model evaluation, refinement
 df= df._get_numeric_data()
 df.head()
+
+#split data into training and testing sets
+#I will use 20 percent of the data for testing
+y_data = df['price']
+x_data = df.drop('price', axis = 1)
+from sklearn.model_selection import train_test_split
+x_train, x_test,y_train, y_test = train_test_split(x_data, y_data,test_size = 0.20,random_state = 1)
+x_test.shape[0]
+x_train.shape[0]
